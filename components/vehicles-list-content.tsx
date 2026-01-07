e8b22d53152b15966be47716a77abce073376f6d
Majkl78 <110742645+Majkl78@users.noreply.github.com>
2026-01-07 18:34:04 +0100

    Update vehicles-list-content.tsx

--- components/vehicles-list-content.tsx
+++ components/vehicles-list-content.tsx
@@ -298,9 +298,13 @@ export default function VehiclesListContent() {
                     </div>
                   </div>
                   
-                  <h3 className="text-2xl font-bold text-slate-900 mb-2">
-                    {vehicle.licencePlate}
-                  </h3>
+                 <h3 className="text-2xl font-bold text-slate-900 mb-2">
+  <Link href={`/vehicles/${vehicle._id}`} className="hover:underline">
+    {vehicle.licencePlate}
+  </Link>
+</h3>
+
+
                   
                   <div className="space-y-2 text-sm">
                     <div>
